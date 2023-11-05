22acdc4053a5cf7474022aa7b61cdd320bdf325c
andresjaromezuk <andres.jaromezuk@gmail.com>
2023-11-05 12:14:57 -0300

    getProducts, getProductById, addProduct, updateProduct added

---
+++ desafio-4/src/app.js
@@ -0,0 +1,16 @@
+import ProductManager from './services/productManager.js'
+import express, { json, urlencoded } from 'express'
+import {apiRouter} from './router/apiRouter.js'
+
+//Express
+const app = express()
+const PORT = 8080
+
+//Middlewares 
+app.use(urlencoded({ extended: true }))
+app.use(json())
+
+//Rutas
+app.use('/api', apiRouter)
+
+app.listen(PORT, ()=>{console.log(`Servidor escuchando en puerto ${PORT}`)})
