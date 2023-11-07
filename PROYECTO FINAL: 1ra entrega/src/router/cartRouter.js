eaec874464fd35603a74635c6974bf6e7b3c9844
andresjaromezuk <andres.jaromezuk@gmail.com>
2023-11-06 22:50:14 -0300

    adding delete img and readme

---
+++ PROYECTO FINAL: 1ra entrega/src/router/cartRouter.js
@@ -0,0 +1,36 @@
+import { Router } from 'express'
+import CartManager from '../services/cartManager.js'
+const cart_manager = new CartManager('./db/carrito.json')
+
+export const cartRouter = Router()
+
+//Crear carrito
+cartRouter.post('/', async (req, res) => {
+    try {
+        const cart = await cart_manager.createCart()
+        return res.status(200).json({status: "Success", payload: cart})
+    } catch (error) {
+        res.status(500).json({status: "Error", error: error.message})
+    }
+})
+
+//Obtener carrito
+cartRouter.get('/:cid', async (req, res) => {
+    try {
+        const {cid} = req.params
+        const cart = await cart_manager.getCartById(Number(cid))
+        return res.status(200).json({status: "Success", payload: cart})
+    } catch (error) {
+        res.status(404).json({status: "Error", error: error.message})
+    }
+})
+
+//Agregar productos al carrito
+cartRouter.post('/:cid/product/:pid', async (req, res) => {
+    try {      
+        const cart = await cart_manager.addProductToCart(req)
+        return res.status(200).json({status: "Success", payload: cart})
+    } catch (error) {
+        res.status(500).json({status: "Error", error: error.message})
+    }
+})
