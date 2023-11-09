664e4c658c9ff47babf0663b192f0b57665de06c
andresjaromezuk <andres.jaromezuk@gmail.com>
2023-11-09 19:11:17 -0300

    delete desafíos

--- desafio-5/src/router/apiRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import {productRouter} from './productRouter.js'
-import {cartRouter} from './cartRouter.js'
-
-export const apiRouter = Router()
-
-apiRouter.use('/products', productRouter)
-
-apiRouter.use('/carts', cartRouter)
